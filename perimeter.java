a4e809228070c6bcd73a344e52b34282324dba32
simplepradhan <simplepradhan2004@gmail.com>
2025-02-19 21:33:25 +0530

    Add files via upload

---
+++ perimeter.java
@@ -0,0 +1,12 @@
+public class perimeter{
+    static void perimeter(int length , int breadth){
+        int perimeter = 2*(length+breadth);
+        System.out.println("The perimeter of the given rectangle of lenght"+lenght +"and  breadth"+ breadth + "="+ perimeter);
+    }
+    public static void main (String[]args)
+    {
+        int length = 10 ;
+        int breadth = 20 ;
+        perimeter(lenght, breadth);
+    }
+}
