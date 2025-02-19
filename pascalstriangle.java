65c315a639d5552b2fa6bed4fedb6a46b2bdbe9f
simplepradhan <simplepradhan2004@gmail.com>
2025-02-19 22:09:45 +0530

    Add files via upload

---
+++ pascalstriangle.java
@@ -0,0 +1,13 @@
+public class pascalstriangle{
+    public static void main(String[]args){
+        System.out.println("     1");
+        System.out.println("    1 1");
+        System.out.println("   1 2 1");
+        System.out.println("  1 3 3 1");
+        System.out.println(" 1 4 6 4 1");
+        System.out.println("1 5 10 10 5 1");
+               
+
+        
+    }
+}
