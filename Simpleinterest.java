83c93df9e5737f03a87cb6d1de84fcf286eed9e6
simplepradhan <simplepradhan2004@gmail.com>
2025-02-19 21:11:26 +0530

    Add files via upload

---
+++ Simpleinterest.java
@@ -0,0 +1,9 @@
+public class Simpleinterest{
+    public static void main(String[]args)
+    {
+        float P= 20,R=10,T=5;
+        float SI = (P * T * R)/100;
+        System.out.println("Simple interest ="+ SI);
+
+    }
+}
